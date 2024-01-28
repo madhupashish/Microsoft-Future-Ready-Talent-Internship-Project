f882ed445aaacd5e9a1cc0158021d2a1c431909e
Ashish Madhup <119279720+madhupashish@users.noreply.github.com>
2024-01-28 19:44:45 +0530

    Add files via upload

---
+++ ecommercewebbot2-bot-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\ecommercewebbot2-bot-9296-Web-Deploy.pubxml -p:Password=4hWMawCDJsNqequTezPl1EoxWgmjAcFZw5XcKBhjyqThlHtxaEppDpYcTygx
+
