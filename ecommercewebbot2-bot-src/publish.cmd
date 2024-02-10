f3082116060374682f641bdceed45a8efaf26ff6
Ashish Madhup <119279720+madhupashish@users.noreply.github.com>
2024-02-11 01:24:20 +0530

    Delete ecommercewebbot2-bot-src directory

--- ecommercewebbot2-bot-src/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\ecommercewebbot2-bot-9296-Web-Deploy.pubxml -p:Password=4hWMawCDJsNqequTezPl1EoxWgmjAcFZw5XcKBhjyqThlHtxaEppDpYcTygx
-
